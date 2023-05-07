b3adcf2f9b7c0057bbcd13777112c4996d9a4af7
Will <50217731+KvWIlY@users.noreply.github.com>
2023-05-07 17:34:51 -0300

    Update exec.py

--- exec.py
+++ exec.py
@@ -7,8 +7,3 @@ for i in range(13):
     saida, _ = processo.communicate()
     # Converte a saída para um número de ponto flutuante em milissegundos e adiciona à lista de tempos
     tempos.append(float(saida.decode('utf-8')))
-
-# Calcula a média dos tempos capturados
-media = sum(tempos) / len(tempos)
-
-print("A média dos tempos é:", media, "milissegundos")
